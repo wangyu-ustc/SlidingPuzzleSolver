f8e7c702db2d56f7ae6fa7fb4c82db4dad1a44fa
wangyu-ustc <1294837640@qq.com>
2023-03-09 20:48:17 -0800

    initialize code

---
+++ lab1p1vs/lab1p1vs.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <ctime>
+#include "solutionIDAstar.h"
+#include "solution.h"
+using namespace std;
+
+int main()
+{
+    solution sol;       //使用A*算法
+    //solutionIDA* sol  //使用IDA*算法
+    int* target_pos=new int[22]{ 0,
+        0, 1, 2, 3, 4,
+        10, 5, 7, 8, 9,
+        12, 13, 14, 15, 16,
+        17, 18, 19, 20, 21,
+        22 };
+    status s("input3.txt", nullptr);
+    s.set_target(target_pos);
+    s.print();
+    int starttime = clock();
+    s = sol.solve(s);
+    int endtime = clock();
+    std::cout << "Time : " <<endtime-starttime<<"ms" <<std::endl;
+    delete[] target_pos;
+    return 0;
+}
+
+/*
+0, 1, 2, 3, 4,
+        10, 5, 7, 8, 9,
+        12, 13, 14, 15, 16,
+        17, 18, 19, 20, 21,
+        22 };
+*/
